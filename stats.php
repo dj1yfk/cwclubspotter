995085db81e426f33951ef2647cbfff57954dc4c
Fabian Kurz <fabian@fkurz.net>
2020-08-21 20:24:44 +0200

    rbn stats: adjust scaling

--- stats.php
+++ stats.php
@@ -29,17 +29,19 @@ display: block;
     foreach ($data as $d) {
         list($ts, $c, $u) = preg_split("/\s+/", $d);
 
-        $scale = 40;
+        $scale = 80;
         $height_count = ($c - $u) / $scale;
         $height_unique = $u / $scale; 
 
-        echo "<td title='".gmstrftime('%A %d-%b-%y %T %Z', $ts)." - $c / $u'><img src='act/img/2.png' width=2 height=$height_count><img src='act/img/5.png' width=2 height=$height_unique></td>";
+        echo "<td title='".gmstrftime('%A %d-%b-%y %T %Z', $ts)." - $c / $u'><img src='act/img/2.png' width=4 height=$height_count><img src='act/img/5.png' width=4 height=$height_unique></td>";
     }
 
 ?>
 </tr>
 </table>
 
+<br>
+Data shown: Number of spots on the bandmap, number of unique calls, for the past 60 minutes. Hover over graph to see the time stamps.
 
 
 </html>

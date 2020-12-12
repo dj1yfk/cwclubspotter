b60fbb80295cce9907dfa101768c991ee3fcd0ae
Fabian Kurz <fabian@fkurz.net>
2020-12-12 15:35:27 +0100

    add externally embeddable version for UFT

---
+++ ext/uft.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<head>
+<link rel="stylesheet" href="https://rbn.telegraphy.de/bandmap_ext.css">
+</head>
+<body>
+<div class="bandmap">
+    <div id="tab"></div>
+    <div id="upd"></div>
+</div>
+<script src="https://rbn.telegraphy.de/js/uft.js"></script>
+</body>

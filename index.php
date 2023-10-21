96897361c8f597a6a16c309055fe032f71ad0de6
Fabian Kurz <fabian@fkurz.net>
2023-10-21 11:16:40 +0200

    bandmap: by default (no cookie), do not set any club filter

--- index.php
+++ index.php
@@ -365,7 +365,7 @@ include("js/bm_alerts.js");
 			document.getElementById('cb' + bandName[i]).checked = (getCookie(bandName[i])==null || getCookie(bandName[i])=='true');
 		}
 		for (i = 0; i < clubName.length; i++) {
-			document.getElementById('cb' + clubName[i]).checked = (getCookie(clubName[i])==null || getCookie(clubName[i])=='true');
+			document.getElementById('cb' + clubName[i]).checked = getCookie(clubName[i])=='true';
 		}
 		for (i = 0; i < speedName.length; i++) {
 			document.getElementById('cb' + speedName[i]).checked = (getCookie(speedName[i])==null || getCookie(speedName[i])=='true');

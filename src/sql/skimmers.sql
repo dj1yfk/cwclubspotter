33e88a0d6e10944829f72c1f8d827b8f473e7ec2
Fabian Kurz <fabian@fkurz.net>
2023-11-29 22:04:32 +0100

    add forgotten skimmers.sql table definition for custom filter

---
+++ src/sql/skimmers.sql
@@ -0,0 +1,6 @@
+drop table skimmers;
+CREATE TABLE `skimmers` (
+        `callsign` varchar(64) NOT NULL default '',
+        `lat` float not null default 0,
+        `lng` float not null default 0
+);

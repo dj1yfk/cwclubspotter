dc1b7455aba18ec7082c9df00f0205ec6faa8fa3
Fabian Kurz <fabian@fkurz.net>
2026-01-27 21:38:01 +0100

    Add simple locking to RBN statistics image generation.
    Only one image at a time, others have to wait. This was made necessary
    after some "AI" bots brought the server down with hundreds of request.
    
    I still have to figure out what the "I" in "AI" really means.

--- act/img.php
+++ act/img.php
@@ -17,17 +17,36 @@ if (preg_match('/^[a-z0-9\/ ]+$/i', $call)) {
     # error_log($filename);
 
     if (time()-filemtime('/tmp/rbn2_cache/'.$filename.'-small.png') > 1 * 3600) {   // 1h
-        # error_log("generating picture for $filename");
+        error_log("RBNimg.php: generating picture for $filename");
         if (!file_exists("/tmp/rbn2_cache")) {
             mkdir("/tmp/rbn2_cache");
         }
+
+        $lock_count = 0;
+        while (file_exists("/tmp/rbn2_cache/lock")) {
+            $f = file_get_contents("/tmp/rbn2_cache/lock");
+            error_log("RBNimg.php: $call: locking due to: $f (lock count: $lock_count)");
+            sleep(1);
+            $lock_count++;
+            if ($lock_count > 5) {
+                error_log("$cs: lock_count exceeds 5. Sending error.png");
+                $data = file_get_contents("/home/fabian/rbn.telegraphy.de/error.png");
+                header("Content-type: image/png");
+                echo $data;
+                exit();
+            }
+        }
+        
+        file_put_contents("/tmp/rbn2_cache/lock", $call);
         system("xvfb-run -a --server-args=\"-screen 0, 1280x768x24\" wkhtmltoimage -q --width 870 --javascript-delay 500 https://rbn.telegraphy.de/activity_iframe/$call /tmp/rbn2_cache/$filename.png 2> /dev/null");
+        unlink("/tmp/rbn2_cache/lock");
+
         system("convert /tmp/rbn2_cache/$filename.png /tmp/rbn2_cache/$filename-small.png");
         system("rm -f /tmp/rbn2_cache/$filename.png");
-#        error_log("RBNimg.php: $call");
+        error_log("RBNimg.php: $call");
     } 
     else {
-#        error_log("RBNimg.php: $call (cached)");
+        error_log("RBNimg.php: $call (cached)");
     }
 
     $data = file_get_contents("/tmp/rbn2_cache/$filename-small.png");
